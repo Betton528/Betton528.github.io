51b5cc37a542d82a9307ebc4b783a0f0ed946ec8
Betton528 <betton528@wearescpps.org>
2025-04-29 16:08:38 +0000

    changes

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -43,7 +43,9 @@ physikz.updatePosition(circles[1]);
     
         // TODO 7 : Use a loop to create multiple circles
 
-
+        for (var i = 0; i < 100; i++) {
+            drawCircle();
+        }
 
 
         ///////////////////
@@ -57,20 +59,15 @@ physikz.updatePosition(circles[1]);
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
-        physikz.updatePosition(circles[0]);
-        physikz.updatePosition(circles[1]);    
-        physikz.updatePosition(circles[2]);
-        physikz.updatePosition(circles[3]);
-        physikz.updatePosition(circles[4]);
+        
 
             // TODO 5 : Call game.checkCirclePosition() on your circles
-           game.checkCirclePosition(circles[0])
-           game.checkCirclePosition(circles[1])
-           game.checkCirclePosition(circles[2])
-           game.checkCirclePosition(circles[3])
-           game.checkCirclePosition(circles[4])
-            // TODO 8 / TODO 9 : Iterate over the array
            
+            // TODO 8 / TODO 9 : Iterate over the array
+            for (var i = 0; i < circles.length; i++) {
+                game.checkCirclePosition(circles[i])
+                physikz.updatePosition(circles[i]);
+              }
             
         }
     
@@ -87,8 +84,17 @@ physikz.updatePosition(circles[1]);
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            if ( circle.x < 0 ) {
+                circle.x = canvas.width;
+            }
 
+            if ( circle.y > canvas.height ) {
+                circle.y = 0;
+            }
+           
+            if ( circle.y < 0 ) {
+                circle.y = canvas.height;
+            }
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
